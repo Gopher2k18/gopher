9d89de327c6a69f22bc4711ffb0e5fc05aeb6484
juhavuo <juha.vuokko@pp.inet.fi>
2018-11-23 01:35:48 +0200

    little change to backendconnector service

--- src/app/services/backendconnector.service.ts
+++ src/app/services/backendconnector.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http'
+import {Connection} from '../models/connection';
 
 @Injectable()
 export class BackendconnectorService {
 
-  slackMessagesUrl = 'https://gopher-backend.herokuapp.com/events'
+  slackMessagesUrl = Connection.address;
 
   constructor(private http: HttpClient) { }
 
