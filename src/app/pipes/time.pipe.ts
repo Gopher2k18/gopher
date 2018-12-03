09d5ac50e07361f56f23701572a0b7e262ec95d8
juhavuo <juha.vuokko@pp.inet.fi>
2018-12-03 10:30:51 +0200

    corrections how date is shown in feed html

--- src/app/pipes/time.pipe.ts
+++ src/app/pipes/time.pipe.ts
@@ -9,7 +9,7 @@ export class TimePipe implements PipeTransform {
   constructor(private datePipe: DatePipe){}
 
   transform(value: string): string {
-    return  this.datePipe.transform(new Date(Number(value)*1000),'hh:mm');;
+    return  this.datePipe.transform(new Date(Number(value)*1000),'HH:mm');;
   }
 
 }
