412e1e5c57a37a4c2cc0de92d0b7ecef3535e4f6
cdrbbt <egor.bykadorov@gmail.com>
2018-12-05 13:25:27 +0200

    card for empty list

--- src/app/components/starred/starred.component.ts
+++ src/app/components/starred/starred.component.ts
@@ -1,5 +1,4 @@
 import { Card } from './../../models/card';
-import { Message } from './../../models/message';
 import { BackendconnectorService } from './../../services/backendconnector.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -13,19 +12,13 @@ export class StarredComponent implements OnInit {
 
 
   messages: Card[] = [];
-  filteredmessages: Message[] = [];
+  empty: Boolean = false;
 
   constructor(private backendconnectorService: BackendconnectorService,
     private router: Router) { }
 
-  selectedMessage: Message;
-
-  onSelect(message: Message): void {
-    this.selectedMessage = message;
-    this.selectedMessage.favourite = !this.selectedMessage.favourite;
-  }
-
   ngOnInit() {
     this.messages = this.backendconnectorService.fetchStars();
+    this.empty = this.messages.length ? false : true;
   }
 }
